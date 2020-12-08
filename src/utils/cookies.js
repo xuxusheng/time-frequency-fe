68591d35a6dfac5287dad4d8d74bf9b79e77d449
Verahuan <1611247549@qq.com>
2020-12-08 21:00:02 +0800

    添加element ui message提示信息 使用Vue 的全局变量 cookies.js的使用（添加头信息）

--- src/utils/cookies.js
+++ src/utils/cookies.js
@@ -1,16 +1,14 @@
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
 
 const auth = {
-    setCookies: function (name, value, obj) {
-        return Cookies.set(name, value, obj)
-
-    },
-    getCookies: function (name) {
-        return Cookies.get(name)
-
-    },
-    removeCookies: function (name) {
-        return Cookies.remove(name)
-    }
+  setCookies: function(name, value, obj) {
+    return Cookies.set(name, value, obj);
+  },
+  getCookies: function(name) {
+    return Cookies.get(name);
+  },
+  removeCookies: function(name) {
+    return Cookies.remove(name);
+  }
 };
-export {auth}
+export { auth };
