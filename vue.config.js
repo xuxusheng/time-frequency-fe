2f87cfcedb08222c3265d43ffc1001846b3e77fa
hxy941025 <562450613@qq.com>
2020-12-09 16:24:52 +0800

    feat:add router

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+module.exports = {
+  lintOnSave: false,
+  devServer: {
+    port: 8080,
+    open: true
+  },
+  configureWebpack: {
+    devtool: "source-map",
+    resolve: {
+      alias: {
+        "@": resolve("src"),
+        "@c": resolve("src/components"),
+        views: resolve("src/views"),
+        api: resolve("src/api")
+      }
+    }
+  }
+};
